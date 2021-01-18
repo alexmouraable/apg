89ad9d49bf06a0dfc3d47d093c45bbbaab9687a4
alexmouraable <alexmoura658@gmail.com>
2021-01-18 10:09:43 -0300

    Project creation

---
+++ src/main/java/br/com/ratel/apg/ApgApplication.java
@@ -0,0 +1,13 @@
+package br.com.ratel.apg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ratel/apg/ApgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ratel.apg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
