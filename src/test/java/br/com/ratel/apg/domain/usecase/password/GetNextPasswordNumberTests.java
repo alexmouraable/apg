66bbbfcefbbe9f0b7777a16b8a39c815e339eca5
alexmouraable <alexmoura658@gmail.com>
2021-01-20 14:13:44 -0300

    Moving password tests to the password package

--- src/test/java/br/com/ratel/apg/domain/usecase/password/GetNextPasswordNumberTests.java
+++ src/test/java/br/com/ratel/apg/domain/usecase/password/GetNextPasswordNumberTests.java
@@ -1,4 +1,4 @@
-package br.com.ratel.apg.domain.usecase;
+package br.com.ratel.apg.domain.usecase.password;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
