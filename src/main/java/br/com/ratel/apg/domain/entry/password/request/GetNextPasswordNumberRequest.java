1517a43cb0540622d7b8db211335381fae153a07
alexmouraable <alexmoura658@gmail.com>
2021-01-22 12:05:10 -0300

    Adding constructor with all arguments to GetNextPasswordNumberRequest

--- src/main/java/br/com/ratel/apg/domain/entry/password/request/GetNextPasswordNumberRequest.java
+++ src/main/java/br/com/ratel/apg/domain/entry/password/request/GetNextPasswordNumberRequest.java
@@ -1,11 +1,18 @@
 package br.com.ratel.apg.domain.entry.password.request;
 
+import javax.validation.constraints.NotNull;
+
 import br.com.ratel.apg.domain.type.PasswordType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetNextPasswordNumberRequest {
+	@NotNull(message = "Tipo de senha é obrigatório")
 	private PasswordType passwordType;
 }
