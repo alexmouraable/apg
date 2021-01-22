f3ac461c79ea528b7c1284ffc626b479fd038d4d
alexmouraable <alexmoura658@gmail.com>
2021-01-22 12:02:45 -0300

    Adding constructor with all arguments in the Password model

--- src/main/java/br/com/ratel/apg/domain/model/Password.java
+++ src/main/java/br/com/ratel/apg/domain/model/Password.java
@@ -3,11 +3,15 @@ package br.com.ratel.apg.domain.model;
 import java.time.LocalDateTime;
 
 import br.com.ratel.apg.domain.type.PasswordType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Password {
 	private Long id;
 	private Integer number;
