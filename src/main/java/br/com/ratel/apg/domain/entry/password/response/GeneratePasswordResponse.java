716686aad544173d444da49feb5aa9391d0b2b7b
alexmouraable <alexmoura658@gmail.com>
2021-01-22 16:48:51 -0300

    Adding constructor with all arguments in GeneratePasswordResponse

--- src/main/java/br/com/ratel/apg/domain/entry/password/response/GeneratePasswordResponse.java
+++ src/main/java/br/com/ratel/apg/domain/entry/password/response/GeneratePasswordResponse.java
@@ -3,11 +3,13 @@ package br.com.ratel.apg.domain.entry.password.response;
 import java.time.LocalDateTime;
 
 import br.com.ratel.apg.domain.type.PasswordType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class GeneratePasswordResponse {
 	private Long id;
 	private Integer number;
