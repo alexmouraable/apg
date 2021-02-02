9f4c15e128ea562bbc809d40b96fb502b5385589
alexmouraable <alexmoura658@gmail.com>
2021-02-02 11:08:34 -0300

    Developing the GetGreaterPasswordNumberData implementation

---
+++ src/main/java/br/com/ratel/apg/infrastructure/jpa/data/password/GetGreaterPasswordNumberDataJpa.java
@@ -0,0 +1,21 @@
+package br.com.ratel.apg.infrastructure.jpa.data.password;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.ratel.apg.domain.constant.PasswordType;
+import br.com.ratel.apg.domain.data.password.GetGreaterPasswordNumberData;
+import br.com.ratel.apg.infrastructure.jpa.repository.PasswordRepository;
+
+@Component
+public class GetGreaterPasswordNumberDataJpa implements GetGreaterPasswordNumberData {
+	@Autowired
+	private PasswordRepository passwordRepository;
+	
+	@Override
+	public Integer execute(PasswordType passwordType, LocalDate date) {
+		return this.passwordRepository.findMaxPasswordNumberByPasswordTypeAndGenerationDate(passwordType, date);
+	}
+}
