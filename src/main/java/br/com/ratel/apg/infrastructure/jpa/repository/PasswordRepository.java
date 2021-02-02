6800f3ae017c7a8e21cb810f49637be2530256f3
alexmouraable <alexmoura658@gmail.com>
2021-02-02 00:23:26 -0300

    Creation of a query method to verify the existence of passwords of a certain type on a certain date

--- src/main/java/br/com/ratel/apg/infrastructure/jpa/repository/PasswordRepository.java
+++ src/main/java/br/com/ratel/apg/infrastructure/jpa/repository/PasswordRepository.java
@@ -1,11 +1,14 @@
 package br.com.ratel.apg.infrastructure.jpa.repository;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.ratel.apg.domain.constant.PasswordType;
 import br.com.ratel.apg.infrastructure.jpa.entity.PasswordEntity;
 
 @Repository
 public interface PasswordRepository extends JpaRepository<PasswordEntity, Long> {
-
+	public boolean existsByPasswordTypeAndGenerationDate(PasswordType passwordType, LocalDate date);
 }
