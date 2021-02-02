3282e42e3899020d8a48624f538da68f5ce1d712
alexmouraable <alexmoura658@gmail.com>
2021-02-02 11:03:13 -0300

    Implementing query to get the highest password number

--- src/main/java/br/com/ratel/apg/infrastructure/jpa/repository/PasswordRepository.java
+++ src/main/java/br/com/ratel/apg/infrastructure/jpa/repository/PasswordRepository.java
@@ -3,6 +3,8 @@ package br.com.ratel.apg.infrastructure.jpa.repository;
 import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.ratel.apg.domain.constant.PasswordType;
@@ -11,4 +13,8 @@ import br.com.ratel.apg.infrastructure.jpa.entity.PasswordEntity;
 @Repository
 public interface PasswordRepository extends JpaRepository<PasswordEntity, Long> {
 	public boolean existsByPasswordTypeAndGenerationDate(PasswordType passwordType, LocalDate date);
+
+	@Query("SELECT MAX(p.number) FROM PasswordEntity p WHERE p.passwordType = :passwordType AND p.generationDate = :date")
+	public Integer findMaxPasswordNumberByPasswordTypeAndGenerationDate(
+			@Param("passwordType") PasswordType passwordType, @Param("date") LocalDate date);
 }
