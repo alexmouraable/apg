2144ac6d2e8d087d954903451e9d5e0ae109fe61
alexmouraable <alexmoura658@gmail.com>
2021-02-02 00:24:54 -0300

    Developing the ExistsPasswordData implementation

---
+++ src/main/java/br/com/ratel/apg/infrastructure/jpa/data/password/ExistsPasswordDataJpa.java
@@ -0,0 +1,21 @@
+package br.com.ratel.apg.infrastructure.jpa.data.password;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.ratel.apg.domain.constant.PasswordType;
+import br.com.ratel.apg.domain.data.password.ExistsPasswordData;
+import br.com.ratel.apg.infrastructure.jpa.repository.PasswordRepository;
+
+@Component
+public class ExistsPasswordDataJpa implements ExistsPasswordData {
+	@Autowired
+	private PasswordRepository passwordRepository;
+
+	@Override
+	public boolean execute(PasswordType passwordType, LocalDate date) {
+		return this.passwordRepository.existsByPasswordTypeAndGenerationDate(passwordType, date);
+	}
+}
