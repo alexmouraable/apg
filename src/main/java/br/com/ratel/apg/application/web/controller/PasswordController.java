ef6e6d7bccbea128503edce1891becffe17ff5f2
alexmouraable <alexmoura658@gmail.com>
2021-02-04 00:11:26 -0300

    Implementing method of printing password in PasswordController

--- src/main/java/br/com/ratel/apg/application/web/controller/PasswordController.java
+++ src/main/java/br/com/ratel/apg/application/web/controller/PasswordController.java
@@ -1,13 +1,28 @@
 package br.com.ratel.apg.application.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.ratel.apg.domain.entry.password.PrintPasswordEntry;
+import br.com.ratel.apg.domain.entry.password.request.PrintPasswordRequest;
 
 @Controller
 @RequestMapping("password")
 public class PasswordController {
+	@Autowired
+	private PrintPasswordEntry printPasswordEntry;
+	
 	@RequestMapping("/print-password")
-	public String goToPrintPasswordPage() {
-		return "password/print-password";
+	public ModelAndView goToPrintPasswordPage(PrintPasswordRequest printPasswordRequest) {
+		return new ModelAndView("password/print-password");
 	}	
+	
+	@PostMapping("/print-password")
+	public ModelAndView printPassword(PrintPasswordRequest printPasswordRequest) {
+		this.printPasswordEntry.execute(printPasswordRequest);
+		return new ModelAndView("redirect:print-password");
+	}
 }
