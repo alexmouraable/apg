b350d975955a2834ea8db1d5bde0f8514690e302
alexmouraable <alexmoura658@gmail.com>
2021-02-04 12:58:45 -0300

    Using the presenter on the controller and adjusting the view

--- src/main/java/br/com/ratel/apg/application/web/controller/PasswordController.java
+++ src/main/java/br/com/ratel/apg/application/web/controller/PasswordController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.ratel.apg.application.presenter.PrintPasswordPresenter;
+import br.com.ratel.apg.application.web.presenter.viewmodel.PrintPasswordScreenViewModel;
+import br.com.ratel.apg.domain.constant.PasswordType;
 import br.com.ratel.apg.domain.entry.password.PrintPasswordEntry;
 import br.com.ratel.apg.domain.entry.password.request.PrintPasswordRequest;
 
@@ -13,16 +16,21 @@ import br.com.ratel.apg.domain.entry.password.request.PrintPasswordRequest;
 @RequestMapping("password")
 public class PasswordController {
 	@Autowired
+	private PrintPasswordPresenter<PrintPasswordScreenViewModel[]> printPasswordPresenter;
+
+	@Autowired
 	private PrintPasswordEntry printPasswordEntry;
-	
+
 	@RequestMapping("/print-password")
 	public ModelAndView goToPrintPasswordPage(PrintPasswordRequest printPasswordRequest) {
-		return new ModelAndView("password/print-password");
-	}	
-	
+		ModelAndView modelAndView = new ModelAndView("password/print-password");
+		modelAndView.addObject("passwordTypes", this.printPasswordPresenter.toViewModel(PasswordType.values()));
+		return modelAndView;
+	}
+
 	@PostMapping("/print-password")
-	public ModelAndView printPassword(PrintPasswordRequest printPasswordRequest) {
+	public String printPassword(PrintPasswordRequest printPasswordRequest) {
 		this.printPasswordEntry.execute(printPasswordRequest);
-		return new ModelAndView("redirect:print-password");
+		return "redirect:print-password";
 	}
 }
