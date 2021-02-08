e54b3937cc44647081d830bba62d99cb4f8a47e5
alexmouraable <alexmoura658@gmail.com>
2021-02-08 16:19:44 -0300

    Adding printer name as a parameter

--- src/main/java/br/com/ratel/apg/domain/printer/PrintOut.java
+++ src/main/java/br/com/ratel/apg/domain/printer/PrintOut.java
@@ -3,5 +3,5 @@ package br.com.ratel.apg.domain.printer;
 import br.com.ratel.apg.domain.type.Report;
 
 public interface PrintOut {
-	public void execute(Report report);
+	public void execute(Report report, String printerName);
 }
